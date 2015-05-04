ddcd5bb9cabb28d26b48c5e2b4d00b81212298cd
vixus0 <vixus0@gmail.com>
2015-05-04 13:43:23 +0100

    updated penta mpv plugin since ytdl is now part of mpv

--- desktop/.config/pentadactyl/plugins/mpvlaunch.js
+++ desktop/.config/pentadactyl/plugins/mpvlaunch.js
@@ -1,31 +1,23 @@
 /*
- * Opens URLs in mpv with youtube-dl
+ * Opens URLs in mpv
  */
 
 let mpv_args = "mpv --no-terminal --force-window";
-let ytdl_args = "youtube-dl -i -g";
 
 function clean(str) {
   return str.replace(/([$`"\\])/g, "\\$1");
 }
 
-function cmd(str, extra_args) {
-  return mpv_args+" --playlist=<("+ytdl_args+" "+extra_args+" '"+clean(str)+"')";
+function mpv(s) {
+  var str = String(s);
+  return io.system(mpv_args+" "+clean(str));
 }
 
-function mpv(e) {
-  return io.system(cmd(e.href, "--no-playlist"));
-}
-
-function mpvp(e) {
-  return io.system(cmd(e.href));
+function mpv_hint(e) {
+  return mpv(e.href);
 }
 
 hints.addMode("m", "Open link in mpv", mpv);
-hints.addMode("M", "Open playlist in mpv", mpvp);
 group.commands.add(["mpv"], "Open current URL in mpv",
-    function(args) { io.system(cmd(buffer.URL, "--no-playlist")); }
-    );
-group.commands.add(["mpvp"], "Open current URL in mpv as playlist",
-    function(args) { io.system(cmd(buffer.URL)); }
+    function(args) { mpv(buffer.URL); }
     );
